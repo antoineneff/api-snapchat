b300375639eed0aef294f3d336b945dac89f94df
Antoine Neff <antoineneff@me.com>
2016-06-26 15:01:16 +0200

    Update user model : don't select password in requests by default and cancel versionning

--- app/models/user.js
+++ app/models/user.js
@@ -4,7 +4,12 @@ var Schema      = mongoose.Schema;
 var UserSchema  = new Schema({
     name: String,
     email: String,
-    password: String
+    password: {
+        type: String,
+        select: false
+    }
+}, {
+    versionKey: false
 });
 
 module.exports = mongoose.model('User', UserSchema);
