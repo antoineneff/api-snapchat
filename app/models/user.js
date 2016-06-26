ee8859b1b2bc0f835c438e875ee1f5cbe8df1619
Antoine Neff <antoineneff@me.com>
2016-06-26 12:34:15 +0200

    Deleted token field in user model, no need

--- app/models/user.js
+++ app/models/user.js
@@ -2,10 +2,9 @@ var mongoose    = require('mongoose');
 var Schema      = mongoose.Schema;
 
 var UserSchema  = new Schema({
-    name: String
+    name: String,
     email: String,
-    password: String,
-    token: String
+    password: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
